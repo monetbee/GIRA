9b7fa19247c3b99bf63787a5d4c4ee2269920fea
monetbee <150294707+monetbee@users.noreply.github.com>
2026-09-26 13:57:36 +0900

    show

--- components/product/shop-all-link.tsx
+++ components/product/shop-all-link.tsx
@@ -1,20 +1,9 @@
-"use client";
-
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import type { MouseEvent } from "react";
 import { shopAllHref } from "@/lib/routes";
 
 export function ShopAllLink() {
-  const router = useRouter();
-
-  function handleClick(event: MouseEvent<HTMLAnchorElement>) {
-    event.preventDefault();
-    router.push(shopAllHref);
-  }
-
   return (
-    <Link href={shopAllHref} onClick={handleClick} className="gira-shop-all">
+    <Link href={shopAllHref} className="gira-shop-all">
       SHOP ALL <span aria-hidden="true">→</span>
     </Link>
   );
